d7bcb4fff2a16d9a7f1bce85211932e97ff75004
dgdshfdjhffafsdgreaafdaf <42490692+dgdshfdjhffafsdgreaafdaf@users.noreply.github.com>
2018-08-18 12:48:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const prefix = '!'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`hello`,"http://twitch.tv/S-F")
+client.user.setGame(`!id`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
