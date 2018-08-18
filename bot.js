eaabad5af290a8bc76d7df1f9d797ef36cfb53cc
dgdshfdjhffafsdgreaafdaf <42490692+dgdshfdjhffafsdgreaafdaf@users.noreply.github.com>
2018-08-18 20:12:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -178,7 +178,7 @@ const devs = ['430038396585377793'];
 client.on('message', message => {
 if(message.content === adminprefix + "restart") {
       if (!devs.includes(message.author.id)) return;
-          message.channel.send(⚠️ **الشخص الذي اعاد تشغيل البوت ${message.author.username}**);
+          message.channel.send( **الشخص الذي اعاد تشغيل البوت ${message.author.username}**);
         console.log(⚠️ جاري اعادة تشغيل البوت... ⚠️);
         client.destroy();
         child_process.fork(__dirname + "bot.js");
