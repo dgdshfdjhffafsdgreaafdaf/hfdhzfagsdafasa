13eab171340721a9b0a04ead8ac452fdec36b8c1
dgdshfdjhffafsdgreaafdaf <42490692+dgdshfdjhffafsdgreaafdaf@users.noreply.github.com>
2018-08-20 13:50:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,20 +66,6 @@ message.channel.send(alpha);
 
 
 
-client.on('message', message => {
-   if (message.content === "myid2") {
-   let embed = new Discord.RichEmbed()
-  .setColor("RANDOM")
-  .setThumbnail(message.author.avatarURL)
-  .addField("Name:",`${message.author.username}`, true)
-  .addField('Discrim:',"#" +  message.author.discriminator, true)
-  .addField("ID:", message.author.id, true)
-  .addField("Create At:", message.author.createdAt, true)
-     
-     
-  message.channel.sendEmbed(embed);
-    }
-});
 
 
 
